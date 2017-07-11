f8da3a6d800bbe1367a7347cd917ee2455b2dd1f
Carl Colglazier <carl@carlcolglazier.com>
2017-07-11 11:19:05 -0400

    Use ISO3 codes as keys.

--- examples/migration/migration.py
+++ examples/migration/migration.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-import data
+import data as data
 from constants import POPULATION_SCALE, MIGRATION_THRESHOLD, PROCESSES, SPLITS
 from gos import Globe
 
@@ -15,16 +15,17 @@ def generate_agents(df, country, population):
     """
     def max_value(attribute):
         return df[attribute].max()
-
-    start = df[df.index < country].Population.sum()
+    # Turn this on for truly random output from each process.
+    # pid = mp.current_process()._identity[0]
+    rand = np.random.mtrand.RandomState(0)
     country_data = df[df.index == country].to_dict("records")[0]
     gdp = country_data["GDP"]
-    income_array = gdp / 10 * np.random.chisquare(10, population).astype('float32')
+    income_array = gdp / 10 * rand.chisquare(10, population).astype('float32')
     unemployment_rate = float(country_data["Unemployment"] / 100.0)
-    employment_array = np.random.choice([True, False], population,
-                                        p=[1 - unemployment_rate, unemployment_rate])
+    employment_array = rand.choice([True, False], population,
+                                   p=[1 - unemployment_rate, unemployment_rate])
     attachment_array = (country_data["Fertility"] *
-                        np.random.triangular(0.0, 0.5, 1.0, population) /
+                        rand.triangular(0.0, 0.5, 1.0, population) /
                         max_value("Fertility")).astype('float32')
     frame = pd.DataFrame({
         "Country": pd.Categorical([country] * population, list(df.index)),
@@ -34,13 +35,13 @@ def generate_agents(df, country, population):
         "Location": pd.Categorical([country] * population, list(df.index)),
         "Migration": 0,
     }, columns=world_columns)
-    frame.index += start
     return frame
 
 
 def migrate_array(a, **kwargs):
     if len(a[a.Migration > MIGRATION_THRESHOLD]) == 0:
         return a.Location
+    np.random.seed(1000)
     migration_map = kwargs["migration_map"]
     countries = kwargs["countries"]
     for country, population in a.groupby("Location"):
@@ -68,13 +69,13 @@ def migrate_score(a, **kwargs):
 
 
 def main():
-    np.random.seed(0)
+    np.random.seed(1000)
     globe = Globe(data.all(), processes=PROCESSES, splits=SPLITS)
 
     globe.create_agents(generate_agents)
 
     globe.agents.Migration = globe.run_par(migrate_score, max_income=globe.agents.Income.max(),
-                                           conflict=globe.df[["Conflict"]],
+                                           conflict=globe.df[["Conflict"]].sort_index(),
                                            max_conflict=globe.df.Conflict.max(),
                                            columns=["Income", "Employed", "Attachment", "Location"])
 
@@ -99,6 +100,7 @@ def main():
     print("Migration model completed at a scale of {}:1.".format(int(1 / POPULATION_SCALE)))
     migrants = globe.agents[globe.agents.Country != globe.agents.Location]
     print("There were a total of {} migrants.".format(len(migrants)))
+    print("There were a total of {} agents.".format(len(globe.agents)))
     changes = (globe.agents.Location.value_counts() -
                globe.agents.Country.value_counts()).sort_values()
     print(changes.head())
